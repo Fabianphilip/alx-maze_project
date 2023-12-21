ff19b7f7a07bdfc4db3ea1737c666aab35ae92b2
Fabian <125877740+Fabianphilip@users.noreply.github.com>
2023-12-21 20:24:47 +0100

    cast.c

--- cast.c
+++ cast.c
@@ -1,5 +1,6 @@
 #include "maze.h"
 
+// Function to check if a given point is a wall in the maze
 bool wall_pun(Vector pt)
 {
 	Vector p;
@@ -11,16 +12,25 @@ bool wall_pun(Vector pt)
 	return false;
 }
 
+// Function to cast rays and determine the distance to the first wall hit
 float casting(Vector rd)
 {
+	// Variables for calculations
 	Vector unitlength, raylength, dir, lasttile;
 	float distance;
 	bool hit;
 	int index_map;
+	char wall;
+
+	// Calculate unit lengths for the x and y directions of the ray
 	unitlength.x = sqrt(1 + (rd.y / rd.x) * (rd.y / rd.x));
 	unitlength.y = sqrt(1 + (rd.x / rd.y) * (rd.x / rd.y));
+
+	// Find the position of the last tile
 	lasttile.x = ((int)pPos.x / boxSize) * boxSize;
 	lasttile.y = ((int)pPos.y / boxSize) * boxSize;
+
+	// Determine the direction and ray length for the x-direction
 	if (rd.x < 0)
 	{
 		dir.x = -1;
@@ -31,6 +41,8 @@ float casting(Vector rd)
 		dir.x = 1;
 		raylength.x = (lasttile.x + boxSize - pPos.x) * unitlength.x;
 	}
+
+	// Determine the direction and ray length for the y-direction
 	if (rd.y < 0)
 	{
 		dir.y = -1;
@@ -41,36 +53,45 @@ float casting(Vector rd)
 		dir.y = 1;
 		raylength.y = (lasttile.y + boxSize - pPos.y) * unitlength.y;
 	}
+
+	// Initialize distance, hit status, map index, and wall type
 	distance = 0;
 	hit = false;
 	index_map = 0;
-	char wall;
+	wall = '\0'; // Initialize wall as null character
+
+	// Ray casting loop
 	for (; !hit && distance < 750;)
 	{
+		// Check whether the next tile in the x or y direction is hit first
 		if (raylength.x < raylength.y)
 		{
 			lasttile.x += dir.x * boxSize;
 			distance = raylength.x;
 			raylength.x += unitlength.x * boxSize;
-			wall = 'h';
+			wall = 'h'; // Wall is horizontal
 		}
 		else
 		{
 			lasttile.y += dir.y * boxSize;
 			distance = raylength.y;
 			raylength.y += unitlength.y * boxSize;
-			wall = 'v';
+			wall = 'v'; // Wall is vertical
 		}
+
+		// Check if the new position hits a wall in the maze
 		if (lasttile.x >= 0 && lasttile.x < width2d && lasttile.y >= 0 && lasttile.y < height)
 		{
 			if (wall_pun(lasttile))
 				hit = true;
 		}
 	}
-	if (wall == 'h')
-		SDL_SetRenderDrawColor(renderer, 71, 5, 154, 255);
-	else
-		SDL_SetRenderDrawColor(renderer, 100, 16, 0, 203);
-	return distance;
-}
 
+	// Set the rendering draw color based on the type of wall hit
+	if (wall == 'h')
+		SDL_SetRenderDrawColor(renderer, 71, 5, 154, 255); // Horizontal wall color
+	else
+		SDL_SetRenderDrawColor(renderer, 100, 16, 0, 203); // Vertical wall color
+
+	return distance; // Return the distance to the first wall hit
+}
