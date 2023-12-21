fe4ff373a5c060f5092a7f64a9812894e759879e
Fabian <125877740+Fabianphilip@users.noreply.github.com>
2023-12-21 20:30:53 +0100

    sdl.c

--- sdl.c
+++ sdl.c
@@ -1,42 +1,66 @@
 #include "maze.h"
 
+// Global variables for SDL window and renderer
 SDL_Renderer *renderer;
 SDL_Window *window;
 
+// Function to initialize SDL
 void sdl_init(void)
 {
-	SDL_Init(SDL_INIT_VIDEO);
-	window = SDL_CreateWindow("SDL Experiment", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_OPENGL);
-	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    // Initialize SDL video subsystem
+    SDL_Init(SDL_INIT_VIDEO);
+
+    // Create SDL window with specified title, position, dimensions, and flags
+    window = SDL_CreateWindow("SDL Experiment", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_OPENGL);
+
+    // Create SDL renderer with hardware acceleration and vertical sync
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 }
 
+// Function to process SDL events
 void process(void)
 {
-	SDL_Event event;
-	const Uint8 *keyStates = SDL_GetKeyboardState(NULL);
-	while (SDL_PollEvent(&event))
-	{
-		if (event.type == SDL_QUIT)
-			running = false;
-	}
-	player_moves(keyStates);
+    SDL_Event event;
+
+    // Get the current state of the keyboard
+    const Uint8 *keyStates = SDL_GetKeyboardState(NULL);
+
+    // Process all SDL events in the event queue
+    while (SDL_PollEvent(&event))
+    {
+        // Check if the event is a window close event
+        if (event.type == SDL_QUIT)
+            running = false;
+    }
+
+    // Process player movements based on keyboard input
+    player_moves(keyStates);
 }
 
+// Function to set the render draw color and clear the renderer
 void clear(void)
 {
-	SDL_SetRenderDrawColor(renderer, 120, 120, 120, 255);
-	SDL_RenderClear(renderer);
+    // Set the render draw color to a light gray
+    SDL_SetRenderDrawColor(renderer, 120, 120, 120, 255);
+
+    // Clear the renderer
+    SDL_RenderClear(renderer);
 }
 
+// Function to present the renderer's contents to the window
 void Screen(void)
 {
-	SDL_RenderPresent(renderer);
+    // Present the contents of the renderer to the window
+    SDL_RenderPresent(renderer);
 }
 
+// Function to clean up SDL resources and exit SDL
 void sdl_exit(void)
 {
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
-	SDL_Quit();
-}
+    // Destroy the renderer and window
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
 
+    // Quit SDL subsystems
+    SDL_Quit();
+}
