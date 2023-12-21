5728c08a7ac5174f36cea7aa8a18f513a46c4b49
Fabian <125877740+Fabianphilip@users.noreply.github.com>
2023-12-21 20:27:20 +0100

    maze.c

--- maze.c
+++ maze.c
@@ -1,31 +1,57 @@
 #include "maze.h"
 
+// Define the width and height of the window
 int width = 1500;
 int width2d = 820;
 int height = 820;
+
+// Define the dimensions of the map and size of each box
 int mapW = 15;
 int mapH = 15;
 int boxSize = 32;
+
+// Flag to control the main loop
 bool running;
 
+// Player position, angle, and direction
 Vector pPos = {70, 70};
 float pA = PI / 2;
 Vector pd = {0, 0};
+
+// Main function
 int main(int argc, char **argv)
 {
-	sdl_init();
-	running = true;
-	while (running)
-	{
-		process();
-		pd.x = cos(pA);
-		pd.y = sin(pA);
-		clear();
-		drawMap();
-		player_drawing();
-		sections_drawing();
-		Screen();
-	}
-	sdl_exit();
-	return (0);
+    // Initialize SDL
+    sdl_init();
+
+    // Set the running flag to true
+    running = true;
+
+    // Main game loop
+    while (running)
+    {
+        // Process input and update game state
+        process();
+
+        // Calculate player direction based on angle
+        pd.x = cos(pA);
+        pd.y = sin(pA);
+
+        // Clear the screen
+        clear();
+
+        // Draw the map, player, and sections
+        drawMap();
+        player_drawing();
+        sections_drawing();
+
+        // Update the screen
+        Screen();
+    }
+
+    // Exit SDL
+    sdl_exit();
+
+    // Return 0 to indicate successful execution
+    return 0;
 }
